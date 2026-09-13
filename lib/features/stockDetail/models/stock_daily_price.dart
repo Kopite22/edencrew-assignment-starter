d1d2a098c1a86af792b626a359dfd5ea38000999
Kopite22 <nulee1000@gmail.com>
2026-09-13 20:03:11 +0900

    [feat] 일별 시세 api 및 class 추가

---
+++ lib/features/stockDetail/models/stock_daily_price.dart
@@ -0,0 +1,17 @@
+class DailyPrice {
+  const DailyPrice({
+    required this.localDate,
+    required this.closePrice,
+    required this.openPrice,
+    required this.highPrice,
+    required this.lowPrice,
+    required this.accumulatedTradingVolume,
+  });
+
+  final String localDate;
+  final int closePrice;
+  final int openPrice;
+  final int highPrice;
+  final int lowPrice;
+  final int accumulatedTradingVolume;
+}
